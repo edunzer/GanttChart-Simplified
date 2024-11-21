e68b8c198ca8266601366b4edb7bdfcb9495a0cd
Ethan Dunzer <ethandunzer@gmail.com>
2024-11-21 15:18:01 -0800

    Remove Color, Effort, Status styling

--- force-app/main/default/lwc/gantt_chart_resource/gantt_chart_resource.js
+++ force-app/main/default/lwc/gantt_chart_resource/gantt_chart_resource.js
@@ -82,51 +82,22 @@ export default class GanttChartResource extends LightningElement {
 
     // Calculate Classes
     let classes = ["slds-is-absolute", "lwc-allocation"];
-    if (allocation.Status__c === "Unavailable") {
-        classes.push("unavailable");
-    } else if (allocation.Status__c === "Hold") {
-        classes.push("hold");
-    } else {
-        switch (allocation.Effort__c) {
-            case "Low":
-                classes.push("low-effort");
-                break;
-            case "Medium":
-                classes.push("medium-effort");
-                break;
-            case "High":
-                classes.push("high-effort");
-                break;
-        }
-    }
 
     // Calculate Styles
     let styles = [
         `left: ${(allocation.left / totalSlots) * 100}%`,
         `right: ${((totalSlots - (allocation.right + 1)) / totalSlots) * 100}%`,
+        "background-color: #1589EE", // Default blue color
         "pointer-events: auto",
         "transition: none"
     ];
-    if (allocation.color && allocation.Status__c !== "Unavailable") {
-        const colorMap = {
-            Blue: "#1589EE",
-            Green: "#4AAD59",
-            Red: "#E52D34",
-            Turqoise: "#0DBCB9",
-            Navy: "#052F5F",
-            Orange: "#E56532",
-            Purple: "#62548E",
-            Pink: "#CA7CCE",
-            Brown: "#823E17",
-            Lime: "#7CCC47",
-            Gold: "#FCAF32"
-        };
-        styles.push(`background-color: ${colorMap[allocation.color]}`);
-    }
 
     // Calculate Label Styles
     let left = allocation.left / totalSlots < 0 ? 0 : allocation.left / totalSlots;
-    let right = (totalSlots - (allocation.right + 1)) / totalSlots < 0 ? 0 : (totalSlots - (allocation.right + 1)) / totalSlots;
+    let right =
+        (totalSlots - (allocation.right + 1)) / totalSlots < 0
+            ? 0
+            : (totalSlots - (allocation.right + 1)) / totalSlots;
     let labelStyle = [
         `left: calc(${left * 100}% + 15px)`,
         `right: calc(${right * 100}% + 30px)`
@@ -138,7 +109,8 @@ export default class GanttChartResource extends LightningElement {
     allocation.labelStyle = labelStyle.join("; ");
 
     return allocation;
-  }
+    }
+
 
 
   // Replace in `setProjects`
